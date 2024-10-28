f67a573c25ab68c3285c9387247c77a9bc976f1b
Minseok Choe <choems@outlook.com>
2024-10-28 12:39:05 +0900

    fix: use proper map for map

--- src/main/scala/esmeta/peval/PartialEvaluator.scala
+++ src/main/scala/esmeta/peval/PartialEvaluator.scala
@@ -24,7 +24,7 @@ import scala.collection.mutable.{Map as MMap, Set as MSet}
 import scala.math.{BigInt as SBigInt}
 import scala.util.{Try, Success}
 
-import scala.collection.immutable.HashMap
+import scala.collection.immutable.SortedMap
 
 /** extensible helper of IR interpreter with a CFG */
 class PartialEvaluator(
@@ -745,12 +745,17 @@ object PartialEvaluator {
     )(using msg: Option[String] = None): SpecializedFuncs = {
 
       // TODO : optimize finding matching overloads
-
-      // ???????
-      val overloadsMap = HashMap.from(overloads.map {
+      val overloadsMap = SortedMap.from(overloads.map {
         case (ol, fa) =>
           (AstValue(fa.params), AstValue(fa.funcBody)) -> ol.name
       })
+      // println(
+      // s"overloadsMap.size == overloads.size: ${overloadsMap.size == overloads.size}",
+      // )
+      // println(
+      // s"overloadsMap.size, overloads.size: ${overloadsMap.size}, ${overloads.size}",
+      // )
+      // assert()
 
       val go =
         (args: Iterable[Value], st: State) =>
@@ -769,10 +774,13 @@ object PartialEvaluator {
                 case (v1: AstValue, v2: AstValue) => Some((v1, v2))
                 case _                            => None
               }
-            fname <- overloadsMap.get(asts)
-            // fname <- overloadsMap
-            // .find(elem => elem._1._1 == asts._1 && elem._1._2 == asts._2)
-            // .map(_._2)
+
+            // k <- overloadsMap.find(elem => elem._1 == asts).map(_._1)
+            // assertion, asts == key then also for .hashCode
+            // _ = assert((asts != k) || (asts.hashCode == k.hashCode))
+            (fname) <-
+              overloadsMap
+                .get(asts)
           } yield fname
       // TODO : optimization ?
       val newGo: PartialFunction[(Iterable[Value], State), String] = {
@@ -783,5 +791,54 @@ object PartialEvaluator {
         Map(FUNC_DECL_INSTANT -> newGo),
       )
     }
+
+    import math.Ordered.orderingToOrdered
+
+    given [T: Ordering]: Ordering[Option[T]] with
+      def compare(x: Option[T], y: Option[T]): Int =
+        (x, y) match {
+          case (Some(a), Some(b)) => a.compare(b)
+          case (Some(_), None)    => 1
+          case (None, Some(_))    => -1
+          case (None, None)       => 0
+        }
+
+    given [T: Ordering]: Ordering[List[T]] with
+      def compare(x: List[T], y: List[T]): Int =
+        (x.sizeCompare(y)) match
+          case 0 =>
+            x.zip(y).foldLeft(0) {
+              case (acc, (a, b)) =>
+                a.compare(b) match
+                  case n if acc == 0 => n
+                  case _             => acc
+            }
+          case n => n
+
+    given Ordering[Ast] with
+      def compare(x: Ast, y: Ast): Int =
+        (x, y) match {
+          case (Lexical(n1, s1), Lexical(n2, s2)) =>
+            n1.compare(n2) match
+              case 0 => s1.compare(s2)
+              case n => n
+          case (Lexical(_, _), Syntactic(_, _, _, _)) => 1
+          case (Syntactic(_, _, _, _), Lexical(_, _)) => -1
+          case (Syntactic(n1, a1, r1, c1), Syntactic(n2, a2, r2, c2)) =>
+            n1.compare(n2) match
+              case 0 =>
+                a1.compare(a2) match
+                  case 0 =>
+                    r1.compare(r2) match
+                      case 0 =>
+                        c1.compare(c2)
+                      case n => n
+                  case n => n
+              case n => n
+        }
+
+    given Ordering[AstValue] with
+      def compare(x: AstValue, y: AstValue): Int =
+        x.ast.compare(y.ast)
   }
 }
