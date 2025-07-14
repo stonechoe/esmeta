add8d5e3e976b643fd35716749c8507103af3990
Minseok Choe <choems@outlook.com>
2025-07-15 03:37:20 +0900

    fixup

--- src/main/scala/esmeta/spec/util/GitHubAction.scala
+++ src/main/scala/esmeta/spec/util/GitHubAction.scala
@@ -9,7 +9,9 @@ object GitHubAction:
 
   extension (elem: Element)
     def startLine: Int = elem.sourceRange().start().lineNumber()
+    def startCol: Int = elem.sourceRange().start().columnNumber()
     def endLine: Int = elem.sourceRange().end().lineNumber()
+    def endCol: Int = elem.sourceRange().end().columnNumber()
 
   def warnYets(spec: Spec, ignoreYets: List[String]): Unit =
     for {
@@ -20,12 +22,18 @@ object GitHubAction:
       if (!ignoreYets.contains(step.toString(detail = true, location = false)))
     } do {
       printWarn(
-        filename = "spec.html",
-        // Not sure but they works when - 1, maybe both 1-based?
-        line = elem.startLine + step.loc.getOrElse(???).start.line - 1, // TODO
-        endLine = elem.startLine + step.loc.getOrElse(???).end.line - 1, // TODO,
-        title = "Unknown Phrase Detected", // TODO
-        message = "This is a newly-introduced syntax which ESMeta cannot understand. Type check in this algorithm will not be performed after this line."  // TODO
+        filename = Some("spec.html"),
+        // -1, cause both 1-based
+        line = Some(elem.startLine + step.loc.getOrElse(???).start.line - 1),
+        endLine = Some(elem.startLine + step.loc.getOrElse(???).end.line - 1),
+        col = Some(elem.startCol + step.loc.getOrElse(???).start.column - 1),
+        endColumn = Some(elem.startCol + step.loc.getOrElse(???).end.column - 1),
+        title = Some("Step Written in Unknown Phrase"), // TODO
+        // TODO
+        message = Some(s"""
+        | This is a syntax which ESMeta cannot understand.
+        | Type checking body of this algorithm (${algo.name}) will not be performed after this line.""".stripMargin.trim()
+        )  // TODO
       )
     }
 
@@ -37,12 +45,26 @@ object GitHubAction:
     * @param endLine
     */
   private def printWarn(
-    filename: String,
-    line: Int,
-    endLine: Int,
-    title: String,
-    message: String
+    filename: Option[String] = None,
+    line: Option[Int]  = None,
+    endLine: Option[Int]  = None,
+    col: Option[Int] = None,
+    endColumn: Option[Int] = None,
+    title: Option[String] = None,
+    message: Option[String] = None
   ): Unit =
-    println(s"::warning file=${filename},line=${line},endLine=${endLine},title=${title}::${message}")
+
+    val args = List(
+      "file" -> filename,
+      "line" -> line,
+      "endLine" -> endLine,
+      "col" -> col,
+      "endColumn" -> endColumn,
+      "title" -> title
+    ).flatMap { case (param, opt) => opt.map(param -> _) }
+    .map { case (param, value) => s"$param=$value"}
+    .mkString(",")
+
+    println(s"::warning ${args}::${message.getOrElse("")}")
     
   
